249e9c84fcde6b3666bcbc3bb4d4330951a60d2b
kurokani <nekolalapiz12@gmail.com>
2025-06-12 13:38:31 +0700

    update minor app

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import streamlit as st
 import joblib
 import numpy as np
 
-# Load model 
+# Load model dan preprocessing
 model = joblib.load("model_xgb.pkl")
 scaler = joblib.load("scaler.pkl")
 selected_features = joblib.load("selected_features.pkl")
 
+# Mapping label hasil prediksi
 label_mapping = {
     0: "Insufficient Weight",
     1: "Normal Weight",
@@ -17,33 +18,39 @@ label_mapping = {
     6: "Obesity Type III"
 }
 
-st.title("💡 Are We Obesity?")
-st.markdown("Input your data to predic")
+st.title("💡 Prediksi Obesitas dengan XGBoost")
+st.markdown("Masukkan data pribadi Anda untuk mengetahui tingkat risiko obesitas.")
 
-# Panel Data Diri
-with st.expander("🧍‍♂️ Data Diri & Fisik", expanded=True):
-    col1, col2 = st.columns(2)
+# Border menggunakan markdown HTML
+st.markdown("""
+<div style="border: 1px solid #4F4F4F; padding: 20px; border-radius: 10px; background-color: #1e1e1e;">
+    <h5 style='color:white;'>🧍‍♂️ Data Diri & Fisik</h5>
+""", unsafe_allow_html=True)
 
-    with col1:
-        gender = st.selectbox("Gender", ["Male", "Female"])
-        age = st.slider("Age", 14, 65, 25)
+# Dua kolom
+col1, col2 = st.columns(2)
 
-    with col2:
-        height = st.number_input("Height(cm)", min_value=1.0, max_value=2.5, value=1.70, step=0.01)
-        weight = st.number_input("Weight(kg)", min_value=30.0, max_value=200.0, value=70.0, step=0.1)
+with col1:
+    gender = st.selectbox("Jenis Kelamin (Gender)", ["Male", "Female"])
+    age = st.slider("Umur (Age)", 14, 65, 25)
 
-    fam_history = st.selectbox("Riwayat keluarga dengan berat badan berlebih?", ["Yes", "No"])
+with col2:
+    height = st.number_input("Tinggi (Height, dalam meter)", min_value=1.0, max_value=2.5, value=1.70, step=0.01)
+    weight = st.number_input("Berat Badan (Weight, dalam kg)", min_value=30.0, max_value=200.0, value=70.0, step=0.1)
 
-# Encode input
+fam_history = st.selectbox("Riwayat keluarga dengan berat badan berlebih?", ["Yes", "No"])
+
+# Tutup div
+st.markdown("</div>", unsafe_allow_html=True)
+
+# Encoding
 gender_encoded = 1 if gender == "Male" else 0
 fam_history_encoded = 1 if fam_history == "Yes" else 0
-
-# Gabungkan sesuai urutan fitur
 input_data = np.array([[age, gender_encoded, height, weight, fam_history_encoded]])
 input_scaled = scaler.transform(input_data)
 
 # Prediksi
-if st.button("🔍 Let's Find out"):
+if st.button("🔍 Prediksi"):
     prediction = model.predict(input_scaled)
     result_label = label_mapping.get(int(prediction[0]), "Tidak diketahui")
-    st.success(f"You are : **{result_label}**")
+    st.success(f"Hasil Prediksi: **{result_label}**")
